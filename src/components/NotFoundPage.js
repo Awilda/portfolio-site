b3b445696744e325384f03367f72231a65adf821
Awilda I Couverthier <awildacouverthier@gmail.com>
2020-05-03 17:15:59 -0400

    fix 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-	<div>
-		404 - <Link to="/">Go Home</Link>
+	<div className="notFoundPage">
+		<h1 className="notFoundPage__errorCode">404</h1>
+		<Link className="notFoundPage__redirectMessage" to="/">visit the homepage here!</Link>
 	</div>
 );
 
